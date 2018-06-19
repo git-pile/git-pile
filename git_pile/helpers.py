552b6b7b45c9025b2b9804aeb772064c518cfdb0
Lucas De Marchi <lucas.demarchi@intel.com>
2018-06-19 01:55:45 -0700

    helpers: allow to wrap a command from env
    It's very common to try to use an environment var to point to optional
    location for binaries and fallback to a default one. Use the same
    wrapper for that.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- git_pile/helpers.py
+++ git_pile/helpers.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 # SPDX-License-Identifier: LGPL-2.1+
 
+import os
+
 from subprocess import run
 
 
 class run_wrapper:
-    def __init__(self, cmd):
-        self.cmd = cmd
+    def __init__(self, cmd, env_default=None):
+        """
+        Wrap @cmd into a cmd() function. If env_default is not None,
+        cmd is considered to be an environment variable and if it's
+        not set, the default value in env_default is used
+        """
+
+        if env_default is not None:
+            val = os.getenv(cmd, env_default)
+        else:
+            val = cmd
+
+        self.cmd = val
 
     def __call__(self, s, *args, **kwargs):
         run([self.cmd] + s.split(), *args, **kwargs)
