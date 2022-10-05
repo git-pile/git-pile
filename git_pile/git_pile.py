git-pile/git-pile

commit c77f2f0766f57b6992d378b8a618028f7a11a613

setup: don't warn on directories and hidden files
Allow any directory in the pile branch without giving a warning. It may
be useful to allow project-specific configuration there.

Also allow any file or directory starting with '.', which is useful when
integrating with CI or other tools that add configuration on hidden
files.

Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

git_pile/git_pile.py
@@ -218,7 +218,7 @@ def assert_valid_pile_branch(pile):
             has_config = True
         elif l == "series":
             has_series = True
-        elif not l.endswith(".patch"):
+        elif not l.endswith(".patch") and not l.startswith('.') and op.dirname(l) != '':
             non_patches = True
 
     errorstr = "Branch '{pile}' does not look like a pile branch. No '{filename}' file found."

commit 359ca73e4e0850062319e7266d2a792affec40dc

pile: Add method Pile.validate_structure()
And update git_pile.py to use it.

Note that Pile.validate_structure() takes a slightly different approach
from assert_valid_pile_branch(): it iterates only direct children of the
tree.

By looking at the commit message for c77f2f0766f5 ("setup: don't warn on
directories and hidden files"), it seems that `op.dirname(l) != ""`
should actually be `op.dirname(l) == ""`, given the intention to allow
directories containing project-specific stuff.

In that case, there is no need to recurse into sub-trees, just checking
if a child is a tree and ignoring it is enough.

Fixes: c77f2f0766f5 ("setup: don't warn on directories and hidden files")
Signed-off-by: Gustavo Sousa <gustavo.sousa@intel.com>

git_pile/git_pile.py
@@ -31,6 +31,7 @@ from .helpers import (
     set_fatal_behavior,
     warn,
 )
+from .pile import Pile, PileError
 
 try:
     import argcomplete
@@ -314,30 +315,6 @@ def get_branch_from_remote_branch(remote_branch):
     return None
 
 
-def assert_valid_pile_branch(pile):
-    # --full-tree is necessary so we don't need "-C gitroot"
-    out = git(f"ls-tree -r --name-only --full-tree {pile}").stdout
-    has_config = False
-    has_series = False
-    non_patches = False
-
-    for l in out.splitlines():
-        if l == "config":
-            has_config = True
-        elif l == "series":
-            has_series = True
-        elif not l.endswith(".patch") and not l.startswith(".") and op.dirname(l) != "":
-            non_patches = True
-
-    errorstr = "Branch '{pile}' does not look like a pile branch. No '{filename}' file found."
-    if not has_series:
-        fatal(errorstr.format(pile=pile, filename="series"))
-    if not has_config:
-        fatal(errorstr.format(pile=pile, filename="config"))
-    if non_patches:
-        warn("Branch '{pile}' has non-patch files".format(pile=pile))
-
-
 def assert_valid_result_branch(result_branch, baseline):
     try:
         git(f"rev-parse {baseline}", stderr=nul_f)
@@ -475,8 +452,13 @@ def cmd_setup(args):
         local_result_branch = git(f"-C {gitroot} symbolic-ref --short -q HEAD").stdout.strip()
         create_result_branch = False
 
-    # content of the pile branch looks like a pile branch?
-    assert_valid_pile_branch(args.pile_branch)
+    pile = Pile(rev=args.pile_branch)
+    try:
+        # structure of the pile branch looks like a pile branch?
+        pile.validate_structure()
+    except PileError as e:
+        fatal(f"Branch '{args.pile_branch}' does not look like a pile branch: {e}")
+
     baseline = get_baseline_from_branch(args.pile_branch)
 
     # sane result branch wrt baseline configured?

git_pile/pile.py
@@ -23,6 +23,7 @@ whether it comes from a git tree-ish object or directory in the file system.
 """
 import abc
 import pathlib
+import subprocess
 
 from . import helpers
 
@@ -56,6 +57,55 @@ class Pile:
         self.__path = path
         self.__reader = _RevReader(rev) if rev else _PathReader(path)
 
+    def validate_structure(self, warn_non_patches=True):
+        """
+        Validate the structure of the pile branch.
+
+        Note that the content of files is not checked.
+
+        If a problem is found, ``PileError`` is raised with a message explaining
+        it.
+
+        If ``warn_non_patches`` is true, a warning is issued if non-patch files
+        are found. Note that files under sub-directories or hidden files are
+        ignored and not considered to be non-patch files.
+        """
+        has_non_patches = False
+        missing_files = {"series", "config"}
+
+        for filename, objecttype in self.__reader.ls(include_type=True):
+            if objecttype == "tree":
+                # Allow directories to contain project-specific files
+                continue
+            elif objecttype != "blob":
+                raise PileError(f"unexpected tree entry type for {filename!r} in {self.__loc_phrase()}: {objecttype}")
+
+            if filename.startswith("."):
+                # Allow hidden files to contain project-specific configuration
+                continue
+
+            if filename in missing_files:
+                missing_files.remove(filename)
+            elif not filename.endswith(".patch"):
+                has_non_patches = True
+
+        if missing_files:
+            missing_files_str = ",".join(sorted(missing_files))
+            raise PileError(f"missing files in {self.__loc_phrase()}: {missing_files_str}")
+
+        if has_non_patches and warn_non_patches:
+            helpers.warn(f"non-patch files found in {self.__loc_phrase()}")
+
+    def __loc_phrase(self):
+        if self.__rev:
+            return f"pile revision {self.__rev}"
+        else:
+            return f'pile directory "{self.__path}"'
+
+
+class PileError(Exception):
+    pass
+
 
 _git = helpers.run_wrapper("git", capture=True)
 
@@ -69,14 +119,76 @@ class _FileReader(abc.ABC):
     use them.
     """
 
-    pass
+    @abc.abstractmethod
+    def ls(self, include_type=False):
+        """
+        Yield each entry found in the pile branch.
+
+        By default, the filename for each entry is yielded. If ``include_type``
+        is true, then a 2-element tuple ``(filename, objecttype)`` is yielded,
+        with ``objecttype`` being the git object type for the entry.
+
+        Implementations backed by file system directories must use the same
+        object type that git would use when for the corresponding tree object.
+        If the directory under a git repository, then ignore rules must be
+        obeyed.
+        """
+        raise NotImplementedError()
 
 
 class _PathReader(_FileReader):
     def __init__(self, path):
         self.__path = pathlib.Path(path)
 
+    def ls(self, include_type=False):
+        for p in self.__filter_git_ignored(self.__path.glob("*")):
+            if not include_type:
+                yield p.name
+            else:
+                objecttype = "tree" if p.is_dir() else "blob"
+                yield p.name, objecttype
+
+    def __filter_git_ignored(self, path_iter):
+        try:
+            _git(["-C", str(self.__path), "rev-parse", "--show-toplevel"])
+        except subprocess.CalledProcessError:
+            yield from path_iter
+
+        names = [p.name for p in path_iter]
+
+        try:
+            ignored = set(
+                _git(
+                    ["-C", str(self.__path), "check-ignore", "--stdin", "-z"],
+                    text=True,
+                    input="\x00".join(names),
+                ).stdout.split("\x00")
+            )
+        except subprocess.CalledProcessError as e:
+            if e.returncode == 1:
+                ignored = set()
+            else:
+                raise
+
+        for name in names:
+            if name not in ignored:
+                yield self.__path / name
+
 
 class _RevReader(_FileReader):
     def __init__(self, rev):
         self.__rev = rev
+
+    def ls(self, include_type=False):
+        if include_type:
+            fmt = "%(objecttype) %(path)"
+        else:
+            fmt = "%(path)"
+        out = _git(["ls-tree", "-z", "--full-tree", f"--format={fmt}", self.__rev]).stdout
+
+        for entry in out.rstrip("\x00").split("\x00"):
+            if include_type:
+                objecttype, name = entry.split(" ", maxsplit=1)
+                yield name, objecttype
+            else:
+                yield entry