git-pile/git-pile

commit 23ba85f1ef41dc5289fa72ba86484741a2fb1d14

genbranch: Support caching
Save time and power for users working with a very long series (talking
about thousands of patches here).

An important missing feature here is cache pruning, which should be done
in the near future.

The table below summarizes timing information for an experiment using
this implementation with a pile containing a little more than 3600
patches.

    ======================================================
    Offset     :1:     :2:     :3:     :4:     :5:     avg
    ------------------------------------------------------
        0%  41.427  41.587  41.157  41.228  40.997  41.279
       25%  30.718  30.315  30.886  30.930  30.893  30.749
       50%  19.827  19.937  19.976  20.375  19.799  19.983
       60%  16.000  15.991  16.071  16.090  16.306  16.091
       70%  12.321  12.411  12.316  12.466  12.327  12.368
       80%   8.445   8.327   8.371   8.282   8.432   8.372
       90%   4.651   4.671   4.689   4.716   4.701   4.686
      100%    .805    .797    .799    .808    .799    .802
    ======================================================

The experiment first makes a first run of genbranch to create the cache
file, saving it as the "original cache". Then, for each run, it:

 1) resets the result branch and the pile to its original state and
    replaces the current cache with the "original cache". The cache
    needs to be reset because otherwise columns :2: through :5: would
    all have timings similar to the case of offset=100%.

 2) updates the pile by modifying the "Subject:" line of one patch in the
    series in order to cause the creation of new commits from that point
    forward. The column "Offset" represents the location relative to the
    size of the series where the patch is modified (e.g. 0% means that
    the first patch is modified; 50%, the patch in the middle).

 3) runs genbranch.

Each column ":i:" represents the i-th run for the given offset. The

git_pile/git_pile.py
@@ -32,6 +32,7 @@ from .helpers import (
     warn,
 )
 from .pile import Pile, PileError
+from .genbranch_caching import GenbranchCache
 
 try:
     import argcomplete
@@ -98,6 +99,8 @@ class Config:
     __attr_doc_genbranch_committer_date_is_author_date = "(bool): Set committer date as the author date for generated patches. See --committer-date-is-author-date in GIT-COMMIT(1)"
     __attr_doc_genbranch_user_name = "(string): Name to use as committer when generating the commits"
     __attr_doc_genbranch_user_email = "(string): E-mail to use as committer when generating the commits"
+    __attr_doc_genbranch_use_cache = "(bool): Use cached information to avoid recreating commits"
+    __attr_doc_genbranch_cache_path = "(path): Path (relative to the .git dir) to the cache file for genbranch"
 
     def __init__(self):
         self.dir = ""
@@ -111,6 +114,8 @@ class Config:
         self.genbranch_committer_date_is_author_date = True
         self.genbranch_user_name = None
         self.genbranch_user_email = None
+        self.genbranch_use_cache = True
+        self.genbranch_cache_path = "pile-genbranch-cache.pickle"
 
         s = git(["config", "--get-regexp", "^pile\\.*"], check=False, stderr=nul_f).stdout.strip()
         if not s:
@@ -1490,19 +1495,56 @@ def _genbranch(root, patchesdir, config, args):
 
     if not args.dirty:
         apply_cmd = ["-c", "core.splitIndex=true", "am", "--no-3way", "--whitespace=warn"]
+        # Let's be conservative: any change in the default behavior of genbranch
+        # must cause caching to be disabled.
+        cache_not_allowed_reasons = []
+
         if config.genbranch_committer_date_is_author_date:
             apply_cmd.append("--committer-date-is-author-date")
+        else:
+            cache_not_allowed_reasons.append("config pile.genbranch-commiter-date-is-author-date is false")
+
         if args.fix_whitespace:
             apply_cmd.append("--whitespace=fix")
+            cache_not_allowed_reasons.append("using option --fix-whitespace")
 
         env = os.environ.copy()
         if config.genbranch_user_name:
             env["GIT_COMMITTER_NAME"] = config.genbranch_user_name
+
         if config.genbranch_user_email:
             env["GIT_COMMITTER_EMAIL"] = config.genbranch_user_email
+
+        cache_allowed = len(cache_not_allowed_reasons) == 0
+        if args.use_cache and not cache_allowed:
+            cache_not_allowed_reasons = "; ".join(cache_not_allowed_reasons)
+            warn(f"Caching disabled because of non-default genbranch operation: {cache_not_allowed_reasons}")
     else:
         env = None
         apply_cmd = ["apply", "--unsafe-paths", "-p1"]
+
+    if not args.dirty and args.use_cache and cache_allowed:
+        if not config.genbranch_cache_path:
+            fatal("Missing cache path (config value for pile.genbranch-cache-path is empty)")
+        cache_path = op.join(git_worktree_get_git_dir(), config.genbranch_cache_path)
+        committer_ident = git(["var", "GIT_COMMITTER_IDENT"], env=env).stdout
+        cache = GenbranchCache(cache_path, committer_ident=committer_ident)
+        # Use Pile from a revision if possible, so we do not calculate sha1 for
+        # each patch.
+        pile_for_cache = pile
+        try:
+            if git(["-C", patchesdir, "status", "--porcelain"], stderr=nul_f).stdout.strip() == "":
+                pile_rev = git(["-C", patchesdir, "rev-parse", "HEAD"]).stdout.strip()
+                pile_for_cache = Pile(rev=pile_rev, baseline=args.baseline)
+        except subprocess.CalledProcessError:
+            pass
+
+        effective_baseline, patchlist_offset = cache.search_best_base(pile_for_cache)
+        if patchlist_offset:
+            patchlist = patchlist[patchlist_offset:]
+    else:
+        effective_baseline = pile.baseline()
+        cache = None
 
     # "In-place mode" resets and applies patches directly to working
     # directory.  If conflicts arise, user can resolve them and continue
@@ -1519,9 +1561,9 @@ def _genbranch(root, patchesdir, config, args):
         if not args.branch:
             # use whatever is currently checked out, might as well be in
             # detached state
-            git(f"reset --hard {pile.baseline()}")
+            git(f"reset --hard {effective_baseline}")
         else:
-            git(f"checkout -B {args.branch} {pile.baseline()}")
+            git(f"checkout -B {args.branch} {effective_baseline}")
 
         any_fallback = False
 
@@ -1559,11 +1601,15 @@ pile patches."""
                 "of the pile. Pile needs to be updated to match result branch"
             )
 
+        if cache:
+            cache.update(pile_for_cache, "HEAD")
+            cache.save()
+
         return 0
 
     # work in a separate directory to avoid cluttering whatever the user is doing
     # on the main one
-    with temporary_worktree(pile.baseline(), root) as d:
+    with temporary_worktree(effective_baseline, root) as d:
         branch = args.branch if args.branch else config.result_branch
         path = git_worktree_get_checkout_path(root, branch)
 
@@ -1576,6 +1622,10 @@ pile patches."""
 
         if args.dirty:
             raise temporary_worktree.Break
+
+        if cache:
+            cache.update(pile_for_cache, "HEAD")
+            cache.save()
 
         head = git(["-C", d, "rev-parse", "HEAD"]).stdout.strip()
 
@@ -2096,6 +2146,14 @@ series  config  X'.patch  Y'.patch  Z'.patch
     parser_genbranch.add_argument(
         "-x", "--baseline", help="Ignoring baseline from pile, use whatever provided as argument", default=None
     )
+    parser_genbranch.add_argument("--no-cache", action="store_false", dest="use_cache")
+    parser_genbranch.add_argument(
+        "--cache",
+        help="Use cached information to avoid recreating commits. "
+        f'Default behavior is {"" if config.genbranch_use_cache else "NOT "}to use cache.',
+        dest="use_cache",
+    )
+    parser_genbranch.set_defaults(use_cache=config.genbranch_use_cache)
     parser_genbranch.set_defaults(func=cmd_genbranch)
 
     # format-patch

git_pile/genbranch_caching.py
@@ -1,0 +1,291 @@
+# SPDX-License-Identifier: LGPL-2.1+
+"""
+This module provides the class ``GenbranchCache``, which provides caching
+functionality for genbranch.
+
+The cached information is composed of trees matching pile patches with commit
+objects. This cache is used by ``GenbranchCache.search_best_base()`` to keep a
+genbranch operation from unnecessarily regenerating commits objects. More
+information about the tree structure can be found in the documentation for the
+internal class ``_Node``.
+"""
+import collections
+import logging
+import pathlib
+import pickle
+import subprocess
+import tempfile
+
+from . import helpers
+
+
+class GenbranchCache:
+    """
+    Class responsible for maintaining cache for genbranch.
+
+    The cache is loaded by instantiating the class, can be updated with
+    ``self.update()`` and saved back to the file system with ``self.save()``.
+
+    The method ``self.search_best_base()`` should be used to find the best
+    starting point for applying patches from a pile.
+    """
+
+    def __init__(self, path, committer_ident=None):
+        """
+        Initialize the cache by loading from ``path``.
+
+        If ``path`` does not exist or an error happens during the load, an empty
+        cache is initialized. In the case of the latter, a warning is also
+        issued.
+
+        If passed, ``committer_ident`` must be the output of calling the command
+        "git var GIT_COMMITTER_IDENT". If the parameter is omitted, that command
+        is called internally. This is used to select different trees based on
+        the committer identity (date information in the string is ignored).
+        """
+        self.__path = pathlib.Path(path)
+        self.__cache_data = _CacheData.load(self.__path)
+
+        if not committer_ident:
+            committer_ident = _git(["var", "GIT_COMMITTER_IDENT"]).stdout
+        # Drop the date information from committer_ident
+        self.__committer_ident = committer_ident.strip().rsplit(maxsplit=2)[0]
+
+        if self.__committer_ident not in self.__cache_data.trees:
+            self.__cache_data.trees[self.__committer_ident] = _Node(None)
+        self.__root = self.__cache_data.trees[self.__committer_ident]
+
+    def search_best_base(self, pile, baseline=None):
+        """
+        Search for the best starting point for a genbranch operation.
+
+        The parameter ``pile`` is used to get the series of patches for the
+        genbranch operation. The parameter ``baseline`` defaults
+        ``pile.baseline()`` and the caller can use it to provide an alternative
+        baseline commit.
+
+        This method uses cached information from previous genbranch runs to find
+        the latest patch from a pile series that already has an existing commit.
+
+        The returned value is the tuple ``(commit, offset)``, with ``commit``
+        being the sha-1 hash of the commit to be used as starting point and
+        ``offset`` being the index from which patches from the pile should be
+        really applied. In other words, given ``l`` as the series of patches of
+        ``pile``, the returned value says that genbranch says that the commits
+        for patches in ``l[0:offset]`` already exist and that only the patches
+        in ``l[offset:]`` need to be applied using ``commit`` as starting point.
+
+        If no suitable commit exists, then ``(baseline, 0)`` is returned.
+        """
+        if not baseline:
+            baseline = pile.baseline()
+
+        try:
+            nodes = [self.__root.children[baseline]]
+        except KeyError:
+            return baseline, 0
+
+        for k in (p.sha1() for p in pile.series()):
+            try:
+                nodes.append(nodes[-1].children[k])
+            except KeyError:
+                break
+
+        # Do a binary search to minimize calls to `git cat-file`.
+        # The following algorithm has the following invariants:
+        #
+        #   1) all nodes in ``node[0:base + 1]`` have existing commit objects.
+        #   2) all nodes in ``node[head + 1:]`` do not have existing commit
+        #      objects.
+        #
+        # At the end of the loop, we will have ``base == head``, which means
+        # that any node after ``base`` (or ``head``) will not have existing
+        # commit objects, making ``node[base].commit`` the best existing
+        # baseline for genbranch.
+        base = 0
+        head = len(nodes) - 1
+        while base < head:
+            i = (base + head + 1) // 2
+            try:
+                _git(["cat-file", "-e", nodes[i].commit], stderr=subprocess.DEVNULL)
+            except subprocess.CalledProcessError:
+                head = i - 1
+            else:
+                base = i
+
+        return nodes[base].commit, base
+
+    def update(self, pile, head):
+        """
+        Update the cache for commits created for patches of a pile.
+
+        Given ``head`` as the tip revision for the result of a genbranch
+        operation on ``pile``, this method updates the cache tree to store the
+        commit hashes for each patch in ``pile``'s series.
+        """
+        baseline = pile.baseline()
+        series_hashes = (p.sha1() for p in pile.series())
+        commits = _git(["rev-list", "--reverse", f"{baseline}..{head}"]).stdout.splitlines()
+
+        if baseline not in self.__root.children:
+            self.__root.children[baseline] = _Node(baseline)
+
+        node = self.__root.children[baseline]
+        for patch_sha1, commit in zip(series_hashes, commits):
+            if patch_sha1 not in node.children or node.children[patch_sha1].commit != commit:
+                node.children[patch_sha1] = _Node(commit)
+            node = node.children[patch_sha1]
+
+    def save(self, path=None):
+        """
+        Write cache data back to the file system.
+
+        By default, the same path passed to the constructor is used. The
+        parameter ``path`` can be used to provide an alternative destination.
+        """
+        self.__cache_data.save(pathlib.Path(path or self.__path))
+
+
+_git = helpers.run_wrapper("git", capture=True)
+_logger = logging.getLogger(__name__)
+
+
+class _Node:
+    """
+    The class ``_Node`` is used to represent a node in a cache tree.
+
+    The objective of a cache tree is to match patch files with commit objects in
+    the git repository.
+
+    A path in such a tree represents a (possibly partial) pile revision. Such a
+    path can be represented as ``[b, p[0], p[p_len - 1]]``, with:
+
+      - ``b`` as the node representing the baseline for the pile;
+      - ``p[i]`` as the node representing the ``i``-th patch of the pile;
+      - ``p_len + 1`` as the length of the path. The path may be partially
+        representing the pile revision when ``p_len`` is less than the number of
+        patches in the pile.
+
+    Note that the root node is omitted from such a representation as it is not
+    relevant to the discussion.
+
+    Paths for two different pile revisions will share the first ``k > 1`` nodes
+    if they have identical baselines and have the same first ``k - 1`` patches
+    in their series.
+
+    Except for the root of the tree, each node stores the sha-1 identifier for a
+    commit object in the ``commit`` attribute. For baseline nodes, ``b.commit``
+    refers to the commit object of the baseline; for patch nodes, ``p[i]``
+    refers to the commit object created by applying the respective patch after
+    applying all ``p[:i]`` patches in sequence on top of the baseline.
+
+    As such, the cache tree can be used to avoid recreating commit objects in a
+    genbranch operation. Given a path, the latest node with an existing commit
+    object can be searched and used as base for applying the remaining patches.
+
+    In order to allow building the path for a pile revision, each node has a
+    ``children`` attribute as a ``dict`` mapping the sha-1 hash of each child's
+    patch file to the child node object.
+    """
+
+    def __init__(self, commit):
+        self.children = {}
+        self.commit = commit
+
+
+class _CacheData:
+    SCHEMA_VERSION = 1
+
+    def __init__(self):
+        self.trees = {}
+
+    @classmethod
+    def load(cls, path):
+        try:
+            with open(path, "rb") as f:
+                schema_version, data_bytes = pickle.load(f)
+
+            if schema_version == cls.SCHEMA_VERSION:
+                return pickle.loads(data_bytes)
+            else:
+                _logger.info(f"creating new cache structure because {path} uses a different schema version.")
+        except FileNotFoundError:
+            _logger.info(f"creating new cache structure because {path} does not exist yet.")
+        except Exception as e:
+            _logger.info(f"creating new cache structure because of unknown error loading {path}.")
+            _logger.warning(f"failed to load genbranch cache: {e}")
+            _logger.warning("using a fresh new cache")
+
+        return _CacheData()
+
+    def save(self, path):
+        data_bytes = pickle.dumps(self)
+        # Using dir=output_path.parent to ensure files will be in the same file
+        # system for the atomic replace operation.
+        f = tempfile.NamedTemporaryFile(delete=False, dir=path.parent)
+        try:
+            pickle.dump((self.SCHEMA_VERSION, data_bytes), f)
+        except Exception:  # pragma: no cover
+            f.close()
+            pathlib.Path(f.name).unlink()
+            raise
+        else:
+            f.close()
+            pathlib.Path(f.name).replace(path)
+
+    # We need to implement ``__getstate__()`` and ``__setstate__()`` because
+    # pickle does not cope well with highly nested structures, which would be
+    # the case for a long patch series.
+    def __getstate__(self):
+        state = dict(self.__dict__)
+        state["trees"] = {k: self.__flatten_tree(root) for k, root in state["trees"].items()}
+        return state
+
+    def __setstate__(self, state):
+        state["trees"] = {k: self.__unflatten_tree(flat_data) for k, flat_data in state["trees"].items()}
+        self.__dict__.update(state)
+
+    def __flatten_tree(self, root):
+        stack = collections.deque([[root, list(root.children), -1]])
+        flat_data = []
+        while stack:
+            node, children_keys, child_idx = stack[-1]
+            if child_idx == -1:
+                node_dict = dict(node.__dict__)
+                node_dict.pop("children")
+                flat_data.append(("node-start", node_dict))
+                stack[-1][-1] += 1
+            elif child_idx < len(children_keys):
+                child_key = children_keys[child_idx]
+                child = node.children[child_key]
+                flat_data.append(("node-child", child_key))
+                stack[-1][-1] += 1
+                stack.append([child, list(child.children), -1])
+            else:
+                flat_data.append(("node-end", None))
+                stack.pop()
+        return flat_data
+
+    def __unflatten_tree(self, flat_data):
+        _, node_dict = flat_data[0]
+        root = _Node.__new__(_Node)
+        root.__dict__.update(node_dict)
+        root.children = {}
+        stack = collections.deque([root])
+        for entry_type, entry_data in flat_data[1:]:
+            if entry_type == "node-start":
+                child_key = stack.pop()
+                node_dict = entry_data
+                node = _Node.__new__(_Node)
+                node.__dict__.update(node_dict)
+                node.children = {}
+                stack[-1].children[child_key] = node
+                stack.append(node)
+            elif entry_type == "node-child":
+                key = entry_data
+                stack.append(key)
+            elif entry_type == "node-end":
+                stack.pop()
+            else:
+                raise Exception(f"unexpected entry type: {entry_type}")  # pragma: no cover
+        return root

test/31_genbranch_caching.bats
@@ -1,0 +1,202 @@
+#!/usr/bin/env bats
+setup_file() {
+  bats_require_minimum_version 1.7.0
+  load 'common'
+
+  create_simple_repo "$BATS_FILE_TMPDIR/testrepo"
+}
+
+setup() {
+  git clone --bare "$BATS_FILE_TMPDIR/testrepo" "$BATS_TEST_TMPDIR/remoterepo"
+
+  git clone "$BATS_TEST_TMPDIR/remoterepo" "$BATS_TEST_TMPDIR/testrepo"
+  pushd "$BATS_TEST_TMPDIR/testrepo"
+  git pile init
+  git config pile.genbranch-user-name = "pile bot"
+  git config pile.genbranch-user-email = "git@pi.le"
+  git checkout -b internal
+  git push origin -u --all
+}
+
+# General caching checks
+@test "genbranch-caching-general" {
+  echo "pile 1" > j.txt && git add j.txt && git commit -m "1st commit after baseline"
+  echo "pile 2" > j.txt && git add j.txt && git commit -m "2nd commit after baseline"
+  echo "pile 3" > j.txt && git add j.txt && git commit -m "3rd commit after baseline"
+  echo "pile 4" > j.txt && git add j.txt && git commit -m "4th commit after baseline"
+
+  git pile genpatches -m "First pile commit"
+  run --separate-stderr git pile genbranch -i
+  [ "${lines[0]}" = "Applying: 1st commit after baseline" ]
+  [ "${lines[1]}" = "Applying: 2nd commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 3rd commit after baseline" ]
+  [ "${lines[3]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 4 ]
+
+  tip_first_series=$(git rev-parse HEAD)
+
+  git reset --hard HEAD~2
+
+  echo "pile iii" > j.txt && git add j.txt && git commit -m "iii commit after baseline"
+  echo "pile iv" > j.txt && git add j.txt && git commit -m "iv commit after baseline"
+
+  git pile genpatches -m "Second pile commit"
+  run --separate-stderr git pile genbranch -i
+  [ "${lines[0]}" = "Applying: iii commit after baseline" ]
+  [ "${lines[1]}" = "Applying: iv commit after baseline" ]
+  [ ${#lines[@]} -eq 2 ]
+
+  git -C "$(git config pile.dir)" reset --hard HEAD~1
+  run --separate-stderr git pile genbranch -i
+  [ ${#lines} -eq 0 ]
+  [ $(git rev-parse HEAD) = "$tip_first_series" ]
+
+  # Cases below should trigger a "full genbranch"
+
+  run --separate-stderr git -c pile.genbranch-user-name="pile bot 2" pile genbranch -i
+  [ "${lines[0]}" = "Applying: 1st commit after baseline" ]
+  [ "${lines[1]}" = "Applying: 2nd commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 3rd commit after baseline" ]
+  [ "${lines[3]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 4 ]
+
+  run --separate-stderr git pile genbranch -i --fix-whitespace
+  [ "${lines[0]}" = "Applying: 1st commit after baseline" ]
+  [ "${lines[1]}" = "Applying: 2nd commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 3rd commit after baseline" ]
+  [ "${lines[3]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 4 ]
+  [[ "$stderr" = *"warning: Caching disabled because of non-default genbranch operation: using option --fix-whitespace"* ]]
+
+  run --separate-stderr git -c pile.genbranch-use-cache=false pile genbranch -i
+  [ "${lines[0]}" = "Applying: 1st commit after baseline" ]
+  [ "${lines[1]}" = "Applying: 2nd commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 3rd commit after baseline" ]
+  [ "${lines[3]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 4 ]
+
+  run --separate-stderr git pile genbranch -i --no-cache
+  [ "${lines[0]}" = "Applying: 1st commit after baseline" ]
+  [ "${lines[1]}" = "Applying: 2nd commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 3rd commit after baseline" ]
+  [ "${lines[3]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 4 ]
+
+  run --separate-stderr git -c pile.genbranch-cache-path=other-file.pickle pile genbranch -i
+  [ "${lines[0]}" = "Applying: 1st commit after baseline" ]
+  [ "${lines[1]}" = "Applying: 2nd commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 3rd commit after baseline" ]
+  [ "${lines[3]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 4 ]
+}
+
+# Check that caching works as expected when updating with changes from other
+# developers
+@test "genbranch-caching-remote" {
+  echo "pile 1" > j.txt && git add j.txt && git commit -m "1st commit after baseline"
+  echo "pile 2" > j.txt && git add j.txt && git commit -m "2nd commit after baseline"
+  echo "pile 3" > j.txt && git add j.txt && git commit -m "3rd commit after baseline"
+  echo "pile 4" > j.txt && git add j.txt && git commit -m "4th commit after baseline"
+
+  git pile genpatches -m "First pile commit"
+  git pile genbranch -i
+  git push origin --all -f
+
+  # Do work on another repository: replace commit "2nd commit after baseline"
+  git clone "$BATS_TEST_TMPDIR/remoterepo" "$BATS_TEST_TMPDIR/testrepo2"
+  pushd "$BATS_TEST_TMPDIR/testrepo2"
+  git config pile.genbranch-user-name = "pile bot"
+  git config pile.genbranch-user-email = "git@pi.le"
+  git checkout internal
+  git pile setup origin/pile origin/internal
+  git pile reset
+  git reset --hard internal~3
+  echo "pile ii" > j.txt && git add j.txt && git commit -m "ii commit after baseline"
+  git cherry-pick -X theirs origin/internal~2..origin/internal
+  git pile genpatches -m "Second pile commit"
+  git pile genbranch -i
+  git push origin --all -f
+  popd
+
+  # Update first repository with new changes, then replace commit "3rd commit
+  # after baseline"
+  git remote update
+  git pile reset
+  git reset --hard internal~2
+  echo "pile iii" > j.txt && git add j.txt && git commit -m "iii commit after baseline"
+  git cherry-pick -X theirs origin/internal~1..origin/internal
+  git pile genpatches -m "Third pile commit"
+  run --separate-stderr git pile genbranch -i
+  # Only commit "1st commit after baseline" should be used from the cache at
+  # this point.
+  [ "${lines[0]}" = "Applying: ii commit after baseline" ]
+  [ "${lines[1]}" = "Applying: iii commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 3 ]
+}
+
+# Check that caching works as expected when reordering commits
+@test "genbranch-caching-reorder-commits" {
+  echo "pile 1" > j.txt && git add j.txt && git commit -m "1st commit after baseline"
+  echo "pile 2" > j.txt && git add j.txt && git commit -m "2nd commit after baseline"
+  echo "pile 3" > j.txt && git add j.txt && git commit -m "3rd commit after baseline"
+  echo "pile 4" > j.txt && git add j.txt && git commit -m "4th commit after baseline"
+
+  git pile genpatches -m "First pile commit"
+  run --separate-stderr git pile genbranch -i
+  [ "${lines[0]}" = "Applying: 1st commit after baseline" ]
+  [ "${lines[1]}" = "Applying: 2nd commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 3rd commit after baseline" ]
+  [ "${lines[3]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 4 ]
+
+  # Cache should still be used after reordering commits without modifying the
+  # pile branch.
+  rev=$(git rev-parse HEAD)
+  commits=($(git rev-list --reverse HEAD~4..))
+  git reset --hard HEAD~4
+  git cherry-pick -X theirs "${commits[0]}" "${commits[3]}" "${commits[2]}" "${commits[1]}"
+  run --separate-stderr git pile genbranch -i
+  [ ${#lines} -eq 0 ]
+  [ $(git rev-parse HEAD) = "$rev" ]
+}
+
+# Check that only part of the cache is used after garbage collection
+@test "genbranch-caching-gc" {
+  echo "pile 1" > j.txt && git add j.txt && git commit -m "1st commit after baseline"
+  echo "pile 2" > j.txt && git add j.txt && git commit -m "2nd commit after baseline"
+  echo "pile 3" > j.txt && git add j.txt && git commit -m "3rd commit after baseline"
+  echo "pile 4" > j.txt && git add j.txt && git commit -m "4th commit after baseline"
+
+  git pile genpatches -m "First pile commit"
+  run --separate-stderr git pile genbranch -i
+  [ "${lines[0]}" = "Applying: 1st commit after baseline" ]
+  [ "${lines[1]}" = "Applying: 2nd commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 3rd commit after baseline" ]
+  [ "${lines[3]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 4 ]
+  rev=$(git rev-parse HEAD)
+
+  git reset --hard HEAD~3
+  echo "pile ii" > j.txt && git add j.txt && git commit -m "ii commit after baseline"
+  echo "pile iii" > j.txt && git add j.txt && git commit -m "iii commit after baseline"
+  echo "pile iv" > j.txt && git add j.txt && git commit -m "iv commit after baseline"
+  git pile genpatches -m "Second pile commit"
+  run --separate-stderr git pile genbranch -i
+  [ "${lines[0]}" = "Applying: ii commit after baseline" ]
+  [ "${lines[1]}" = "Applying: iii commit after baseline" ]
+  [ "${lines[2]}" = "Applying: iv commit after baseline" ]
+  [ ${#lines[@]} -eq 3 ]
+
+  # After garbage collection, we expect that the last 3 commits from the first
+  # version of the result branch to be removed, so that only the first patch can
+  # be skipped.
+  git -c gc.reflogExpire=now gc --prune=now
+  ! git cat-file -e $rev
+  git -C "$(git config pile.dir)" reset --hard HEAD~1
+  run --separate-stderr git pile genbranch -i
+  [ "${lines[0]}" = "Applying: 2nd commit after baseline" ]
+  [ "${lines[1]}" = "Applying: 3rd commit after baseline" ]
+  [ "${lines[2]}" = "Applying: 4th commit after baseline" ]
+  [ ${#lines[@]} -eq 3 ]
+}

commit 5d97abbf5fed3f6c5900f8420454cb8a1d6b0c71

genbranch: Add missing action for --cache
Fixes: 23ba85f1ef41 ("genbranch: Support caching")
Signed-off-by: Gustavo Sousa <gustavo.sousa@intel.com>

git_pile/git_pile.py
@@ -2151,6 +2151,7 @@ series  config  X'.patch  Y'.patch  Z'.patch
         "--cache",
         help="Use cached information to avoid recreating commits. "
         f'Default behavior is {"" if config.genbranch_use_cache else "NOT "}to use cache.',
+        action="store_true",
         dest="use_cache",
     )
     parser_genbranch.set_defaults(use_cache=config.genbranch_use_cache)