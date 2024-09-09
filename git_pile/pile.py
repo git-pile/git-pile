git-pile/git-pile

commit a7e0f7f25a876d7b750a532f920b210cee6f0c29

pile: Add method Pile.series()
And refactor git_pile.py to use it when appropriate.

Signed-off-by: Gustavo Sousa <gustavo.sousa@intel.com>

git_pile/pile.py
@@ -126,6 +126,16 @@ class Pile:
         """
         self.__get_config(refresh=True)
 
+    def series(self):
+        """
+        Yield a PilePatch object for each patch found in the pile series.
+        """
+        for line in self.__reader.text("series").splitlines():
+            line = line.strip()
+            if not line or line[0] == "#":
+                continue
+            yield PilePatch(line, self.__reader)
+
     def __loc_phrase(self):
         if self.__rev:
             return f"pile revision {self.__rev}"
@@ -147,6 +157,15 @@ class Pile:
 
         self.__config = data
         return self.__config
+
+
+class PilePatch:
+    def __init__(self, name, reader):
+        self.name = name
+        self.__reader = reader
+
+    def sha1(self):
+        return self.__reader.sha1(self.name)
 
 
 class PileError(Exception):
@@ -190,6 +209,15 @@ class _FileReader(abc.ABC):
         """
         raise NotImplementedError()
 
+    @abc.abstractmethod
+    def sha1(self, *path):
+        """
+        Generate the git sha-1 digest for the file pointed by ``path``.
+
+        This must raise a FileNotFoundError if the file is not found.
+        """
+        raise NotImplementedError()
+
 
 class _PathReader(_FileReader):
     def __init__(self, path):
@@ -205,6 +233,9 @@ class _PathReader(_FileReader):
 
     def text(self, *path):
         return self.__path.joinpath(*path).read_text()
+
+    def sha1(self, *path):
+        return _git(["hash-object", "--", str(self.__path.joinpath(*path))]).stdout.strip()
 
     def __filter_git_ignored(self, path_iter):
         try:
@@ -258,6 +289,10 @@ class _RevReader(_FileReader):
         gitpath = "/".join(path)
         revspec = f"{self.__rev}:{gitpath}"
         return _git(["show", revspec]).stdout
+
+    def sha1(self, *path):
+        _, _, sha1 = self.__ls_path(path)
+        return sha1
 
     def __ls_path(self, path, fmt=""):
         if len(path) == 1:

git_pile/git_pile.py
@@ -1484,11 +1484,7 @@ def _genbranch(root, patchesdir, config, args):
     check_baseline_exists(pile.baseline())
 
     try:
-        patchlist = [
-            op.join(patchesdir, p.strip())
-            for p in open(op.join(patchesdir, "series")).readlines()
-            if len(p.strip()) > 0 and p[0] != "#"
-        ]
+        patchlist = [op.join(patchesdir, p.name) for p in pile.series()]
     except FileNotFoundError:
         patchlist = []
 

commit ef408459fef91e405048b244a8f07ab317da52b1

pile: Pass path components to reader instance
Methods of _FileReader expect path components in positional arguments
and we are passing the full path string to sha1(). Fix that by
extracting the components with pathlib.

Fixes: a7e0f7f25a87 ("pile: Add method Pile.series()")
Signed-off-by: Gustavo Sousa <gustavo.sousa@intel.com>

git_pile/pile.py
@@ -168,7 +168,7 @@ class PilePatch:
         self.__reader = reader
 
     def sha1(self):
-        return self.__reader.sha1(self.name)
+        return self.__reader.sha1(*pathlib.Path(self.name).parts)
 
 
 class PileError(Exception):